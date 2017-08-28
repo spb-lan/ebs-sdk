8176633235ca87eaa76619359c6fdf4afc1c54d0
Ash <ash_williams@mail.ru>
2017-08-28 16:20:50 +0300

    added Reports

---
+++ tests/unit/ReportTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Lan\Ebs\Sdk\Test\Unit;
+
+use Exception;
+use Lan\Ebs\Sdk\Helper\Debuger;
+use Lan\Ebs\Sdk\Report;
+
+class ReportTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+
+    private $client;
+
+    protected function getServices()
+    {
+        return [
+            'di' => new \Codeception\Lib\Di(),
+            'dispatcher' => new \Codeception\Util\Maybe(),
+            'modules' => \Codeception\Util\Stub::makeEmpty('Codeception\Lib\ModuleContainer')
+        ];
+    }
+
+    protected function setUp()
+    {
+        $this->getMetadata()->setServices($this->getServices());
+
+        parent::setUp();
+    }
+
+    protected function _before()
+    {
+        $this->client = ClientTest::getClient();
+    }
+
+    protected function _after()
+    {
+    }
+
+    public function testGetBooksViewStatistics()
+    {
+        $report = new Report($this->client);
+        $stat = $report->getBooksViewsStatistics('month', '2017-07-01', '2017-08-28');
+        $this->assertNotNull($stat);
+        $this->assertNotEmpty($stat);
+
+
+        $stat = $report->getBooksViewsStatistics('month','','');
+        $this->expectException(Exception::class);
+        $this->expectExceptionCode(400);
+
+        $stat = $report->getBooksViewsStatistics('','2017-07-01', '2017-08-28');
+        $this->expectException(Exception::class);
+        $this->expectExceptionCode(400);
+
+        $stat = $report->getBooksViewsStatistics('month','', '2017-08-28');
+        $this->expectException(Exception::class);
+        $this->expectExceptionCode(400);
+    }
+
+    public function testGetJournalsViewsStatistics()
+    {
+        $report = new Report($this->client);
+        $stat = $report->getJournalsViewsStatistics('month', '2017-07-01', '2017-08-28');
+        $this->assertNotNull($stat);
+    }
+
+    public function testGetUsersVisitsStatistics()
+    {
+        $report = new Report($this->client);
+        $stat = $report->getUsersVisitsSatistics('month', '2017-07-01', '2017-08-28');
+        $this->assertNotNull($stat);
+    }
+
+    public function testGetAvailablePackets()
+    {
+        $report = new Report($this->client);
+        $available = $report->getAvailablePackets();
+        $this->assertNotNull($available);
+        $this->assertNotEmpty($available);
+    }
+
+    public function testGetAvailableJournals()
+    {
+        $report = new Report($this->client);
+        $available = $report->getAvailableJournals();
+        $this->assertNotNull($available);
+        $this->assertNotEmpty($available);
+    }
+
+
+    public function testGetAvailableBooks()
+    {
+        $report = new Report($this->client);
+        $available = $report->getAvailableBooks(814392);
+        $this->assertNotNull($available);
+        $this->assertNotEmpty($available);
+    }
+
+}
